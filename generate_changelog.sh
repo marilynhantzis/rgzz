324a1f5b846d551d9b63d1eca846654351d4ff3f
marilynhantzis <ezdramiller@gmail.com>
2024-12-23 02:30:48 +0700

    ну

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -6,6 +6,6 @@ COMMITS=$(git log $LAST_TAG..HEAD --pretty=format:"* %s [%h](https://github.com/
 
 CURRENT_DATE=$(date +%Y-%m-%d)
 
-VERSION="v$(grep -oP 'version=\K\d+\.\d+\.\d+' app.py)" 
+VERSION="v$(grep -oP 'version=\K\d+\.\d+\.\d+' app.py)"  #
 
 echo -e "## $VERSION - $CURRENT_DATE\n$COMMITS\n" >> changelog.md
