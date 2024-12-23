36ac9ab81a0f902138ffbb32b16f3dbc30344efd
marilynhantzis <ezdramiller@gmail.com>
2024-12-23 21:26:44 +0700

    фывыфв

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -4,18 +4,18 @@
 last_tag=$(git describe --tags --abbrev=0)
 
 # Получаем дату для записи в changelog
-current_date=$(date +%Y-%m-%d)
+#current_date=$(date +%Y-%m-%d)
 
 # Получаем список коммитов с момента последнего тега
-commits=$(git log $last_tag..HEAD --pretty=format:"%h - %s")
+#commits=$(git log $last_tag..HEAD --pretty=format:"%h - %s")
 
 # Определяем версию
-version="v$(date +%Y.%m.%d)"  # Формат версии: ГГГГ.ММ.Д
+#version="v$(date +%Y.%m.%d)"  # Формат версии: ГГГГ.ММ.Д
 
 # Создаем новый раздел в changelog
-echo "## [$version] - $current_date" >> changelog.md
+#echo "## [$version] - $current_date" >> changelog.md
 
 # Добавляем коммиты в changelog
-echo "$commits" | while IFS= read -r commit; do
-    echo "- $commit [$(echo $commit | awk '{print $1}')] (https://github.com/marilynhantzis/rgzz/commit/$(echo $commit | awk '{print $1}'))" >> changelog.md
-done
+#echo "$commits" | while IFS= read -r commit; do
+    #echo "- $commit [$(echo $commit | awk '{print $1}')] (https://github.com/marilynhantzis/rgzz/commit/$(echo $commit | awk '{print $1}'))" >> changelog.md
+#done
